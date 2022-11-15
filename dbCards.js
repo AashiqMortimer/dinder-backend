8de00816f7c367f3e4142de6dfb5be60cd463b97
rosalie12e <56639652+rosalie12e@users.noreply.github.com>
2022-11-15 17:05:00 +0000

    dbcard

--- dbCards.js
+++ dbCards.js
@@ -1,9 +1,11 @@
 import mongoose from 'mongoose'
 
 const cardSchema = mongoose.Schema({
+    id: String,
     mealID: String,
     title: String,
     image: String,
+
 })
 
 export default mongoose.model('cards', cardSchema)
