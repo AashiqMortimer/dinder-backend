92e03fb634091b91b5e7eeff9809f6f4b8a1621a
rosalie12e <56639652+rosalie12e@users.noreply.github.com>
2022-11-23 10:42:22 +0000

    dbcard update

--- dbCards.js
+++ dbCards.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose'
 
 const cardSchema = mongoose.Schema({
-    _id: String, //mongo picks this
+    id: String, //mongo picks this
     id: String, // this is the API id
     title: String,
     image: String,
